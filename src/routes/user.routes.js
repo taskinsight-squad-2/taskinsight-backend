d891d8a04b20d26e8dc451746c570fd910d6952a
gilmar.campelo <grc@cesar.org.br>
2026-06-02 14:55:08 -0300

    Ajuste na rota de user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,9 @@
 import { Router } from "express";
-import rateLimit from "express-rate-limit";
 import userController from "../controllers/user.controller.js";
 
 const router = Router();
 
-const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 20,
-  message: { error: "Too many requests, please try again later" },
-});
-
-router.post("/register", authLimiter, userController.register);
-router.post("/login", authLimiter, userController.login);
+router.post("/register", userController.register);
+router.post("/login", userController.login);
 
 export default router;
