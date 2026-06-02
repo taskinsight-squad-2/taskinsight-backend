611d755803786826ce26c21a2f01502d8b2177a5
viniciusthorpe2 <vmat@cesar.org.br>
2026-06-01 21:43:38 -0300

    Delete src/routes/authroutes.js

--- src/routes/authroutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import authController from "../controllers/authcontroller.js";
-
-const router = Router();
-
-router.post("/register", authController.register);
-router.post("/login", authController.login);
-
-export default router;
